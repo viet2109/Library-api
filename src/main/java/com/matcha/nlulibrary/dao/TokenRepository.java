ea9c74a459ee38e7bb426efce05509969e3dcf88
viet0103 <nguyenvanhoang11446@gmail.com>
2023-11-28 14:52:12 +0700

    update querry

--- src/main/java/com/matcha/nlulibrary/dao/TokenRepository.java
+++ src/main/java/com/matcha/nlulibrary/dao/TokenRepository.java
@@ -3,6 +3,7 @@ package com.matcha.nlulibrary.dao;
 import com.matcha.nlulibrary.entity.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,6 +13,6 @@ public interface TokenRepository extends JpaRepository<Token, Integer> {
     select t from Token t join User u on t.user.id = u.id
     where u.id = :userId and (t.expired = false)
 """)
-    List<Token> findAllValidTokensByUser(Integer userId);
+    List<Token> findAllValidTokensByUser(@Param("userId") Integer userId);
     Optional<Token> findByToken(String token);
 }
